92c0bea8b534fb98a7d22c55303a6356d1bdd94d
BHoward <bhoward@runbox.com>
2019-04-03 20:13:07 +0100

    uninstall v15 first too

--- Install-AdalSQL.ps1
+++ Install-AdalSQL.ps1
@@ -43,7 +43,8 @@ if ($PSBoundParameters.ContainsKey('MSIUrl')) {
 
 
 try {
-    $VersionsToUninstall = @{CLSID='4EE99065-01C6-49DD-9EC6-E08AA5B13491'; Version="14 x64"},
+    $VersionsToUninstall = @{CLSID='0127E4A9-98FF-430B-A1B3-08763F9C5F92'; Version="15 x64"},
+                        @{CLSID='4EE99065-01C6-49DD-9EC6-E08AA5B13491'; Version="14 x64"},
                         @{CLSID='EDE51ADE-159F-4EA5-80ED-6C00E9ED4AE7'; Version="13 x64"},
                         @{CLSID='11FA7A0D-8C0B-42A1-94AE-851F1F190A25'; Version="13 x86"}
 
